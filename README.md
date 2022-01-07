8289f771346baf07b820478bf21d20d63b733928
poo <poo1994@hanmail.net>
2022-01-07 13:53:19 +0900

    init setting
    Signed-off-by: poo <poo1994@hanmail.net>

---
+++ README.md
@@ -0,0 +1,23 @@
+## vue 설정
+(Vue.js 개발 생산성을 높여주는 도구 3가지)[https://joshua1988.github.io/web-development/vuejs/boost-productivity/]
+
+## setting
+![image](./img/캡쳐.PNG)
+
+## vue update
+```
+npm update -g @vue/cli
+```
+
+## vue 2 js [Options API] vs [Composition API]
+
+## TypeScript [Class API] vs [Composition API]
+- vue RFC(Request for Comment) : class api 제안을 drop함 ->  Composition API
+- class 대신 setup옵션을 통한 composition api를 지향함(vue 3) - angular 랑비슷
+
+
+## Composition API 
+- 향상된 DOM접근
+- TypeScript 적용에 용이
+- 목적에 맞는 코드를 모듈화하여 진행
+- 유연하고 안전한 확장 가능
---
+++ vue-todo/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": ['error' , {
+      singleQuote: true, //문자열은 작은따옴표
+      semi: true,  //세미콜론
+      useTabs: true, //tab사용
+      trailingComma: 'all', //트레일링콤마 사용
+      tabWidth: 2, //
+      printWidth: 80, //글자길이
+      bracketSpacing: true,
+      arrowParens: 'avoid',
+      endOfLine: 'auto',
+    }] //'off',warn error 등의 경고 레벨이 존재
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
